fddcdce50f67b4f1525f2afa4de09aa1e82c82b5
Jorge Catarino <jorge.catarino@ua.pt>
2019-10-12 00:50:51 +0100

    Add first (maybe final) version of FillReferenceDataBlocks

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
@@ -13,8 +13,28 @@ namespace sofs19
     {
         soProbe(605, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, nbref);
 
-        /* change the following line by your code */
-        return binFillReferenceDataBlocks(ntotal, itotal, nbref);
+        if(nbref > 0){
+            uint32_t freeDBlocks = ntotal - (itotal / IPB) - nbref - 1; // Calculate the free data blocks (Total - InodeBlocks - RefBlocks - root)
+            uint32_t currentRef = HEAD_CACHE_SIZE + nbref + 1; // Calculate the next reference to be written
+            for(uint32_t i = 1; i <= nbref; i++){
+                uint32_t dBlock[RPB];
+                if(i < nbref)
+                    dBlock[0] = i + 1;
+                else
+                    dBlock[0] = NullReference;
+                for(uint32_t z = 1; z <= RPB - 1; z++){
+                    if(currentRef < freeDBlocks){
+                        dBlock[z] = currentRef;
+                        currentRef++;
+                    }
+                    else{
+                        dBlock[z] = NullReference;
+                    }
+                }
+                soWriteRawBlock(i, &dBlock);
+            }
+        }
+        return;
     }
 };
 
