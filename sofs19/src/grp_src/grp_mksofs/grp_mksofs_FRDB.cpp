37ef94ac047d88f83a56277bdb8d860477b92898
Jorge Catarino <jorge.catarino@ua.pt>
2019-10-12 01:01:13 +0100

    Add some commentaries to FRDB

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRDB.cpp
@@ -16,12 +16,17 @@ namespace sofs19
         if(nbref > 0){
             uint32_t freeDBlocks = ntotal - (itotal / IPB) - nbref - 1; // Calculate the free data blocks (Total - InodeBlocks - RefBlocks - root)
             uint32_t currentRef = HEAD_CACHE_SIZE + nbref + 1; // Calculate the next reference to be written
+            
+            // Iterates each available reference block, to fill it.
             for(uint32_t i = 1; i <= nbref; i++){
                 uint32_t dBlock[RPB];
+
                 if(i < nbref)
-                    dBlock[0] = i + 1;
+                    dBlock[0] = i + 1; // Reference to next block on the first space of each reference block.
                 else
-                    dBlock[0] = NullReference;
+                    dBlock[0] = NullReference; // Only applies to the last reference block.
+                
+                // Fills the current block. If currentRef is higher than available free blocks, fills the remaining available spaces with NullReferences.
                 for(uint32_t z = 1; z <= RPB - 1; z++){
                     if(currentRef < freeDBlocks){
                         dBlock[z] = currentRef;
