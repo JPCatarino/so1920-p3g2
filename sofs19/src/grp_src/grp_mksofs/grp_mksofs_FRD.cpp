bbaaa9e9381eb70c5badf2e2b96e186723eed956
Gustavo <gustavoinacio@ua.pt>
2019-10-13 19:40:41 +0100

    Added some of FillRootDir

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
@@ -13,14 +13,27 @@ namespace sofs19
        filling in the contents of the root directory:
        the first 2 entries are filled in with "." and ".." references
        the other entries are empty.
-       If rdsize is 2, a second block exists and should be filled as well.
        */
     void grpFillRootDir(uint32_t itotal)
     {
         soProbe(606, "%s(%u)\n", __FUNCTION__, itotal);
+    
+    SODirEntry dir[DPB];
+
+    dir[0].in=0x00000000;
+    dir[0].name='.';
+
+
+    for(uint32_t i=2; i<DPB; i++) {
+
+        dir[i].in = NullReference;
+        dir[i].name = 
+    }
+
+    soWriteRawBlock();
 
         /* change the following line by your code */
-        return binFillRootDir(itotal);
+        //return binFillRootDir(itotal);
     }
 };
 
