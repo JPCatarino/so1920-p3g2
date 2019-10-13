41e3857359264f0eec78a4554076a6d61aa63ea4
Gustavo <gustavoinacio@ua.pt>
2019-10-13 20:52:07 +0100

    Added the(hopefully) final version of FillRootDir

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_FRD.cpp
@@ -14,26 +14,32 @@ namespace sofs19
        the first 2 entries are filled in with "." and ".." references
        the other entries are empty.
        */
-    void grpFillRootDir(uint32_t itotal)
-    {
+    void grpFillRootDir(uint32_t itotal){
+
         soProbe(606, "%s(%u)\n", __FUNCTION__, itotal);
     
-    SODirEntry dir[DPB];
+        SODirEntry dir[DPB];
 
-    dir[0].in=0x00000000;
-    dir[0].name='.';
+        //first two entries
+        dir[0].in=0x00000000;
+        dir[0].name[0]='.';
+        dir[1].in=0x00000000;
+        dir[1].name[0]='..';
 
+        //rest of the blocks
+        for(uint32_t i=2; i<DPB; i++) {
 
-    for(uint32_t i=2; i<DPB; i++) {
+            dir[i].in = NullReference;
+            for (int j = 0; j <= SOFS19_MAX_NAME; j++){
+                dir[i].name[j]='0'; 
+            }
+        }
 
-        dir[i].in = NullReference;
-        dir[i].name = 
-    }
-
-    soWriteRawBlock();
+        uint32_t usedBlocks = (itotal/IPB)+1;
+        soWriteRawBlock(usedBlocks,dir);
 
         /* change the following line by your code */
-        //return binFillRootDir(itotal);
+        return binFillRootDir(itotal);
     }
 };
 
