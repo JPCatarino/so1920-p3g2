52d738aac8e2b0b009945ab1455cec220091de7d
Jorge Catarino <jorge.catarino@ua.pt>
2019-10-30 01:04:46 +0000

    Add grpTraversePath (Compiles, simple tests run)

--- sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_traverse_path.cpp
@@ -19,8 +19,35 @@ namespace sofs19
     {
         soProbe(221, "%s(%s)\n", __FUNCTION__, path);
 
-        /* change the following line by your code */
-        return binTraversePath(path);
+        // Check if path is absolute
+        if (path[0] != '/'){
+            throw SOException(EINVAL, __FUNCTION__);
+        }
+
+        char *parentDir = dirname(strdupa(path));
+        char *baseName = basename(strdupa(path));
+
+        // If we trasversed whole path, return 0
+        if (strcmp(parentDir, baseName) == 0){
+            return 0; 
+        }
+
+        // Recursively trasverse the path, so we can get to the root
+        uint32_t parentInode = soTraversePath(parentDir);
+        int ih = soOpenInode(parentInode);
+
+        // Checks if we have traverse permissions 
+        if(soCheckInodeAccess(ih, 1)){
+            uint32_t cDirInode = soGetDirEntry(ih, baseName);
+            soCloseInode(ih);
+            // Checks if directory actually exists and/or is actually a directory, if so return inode value
+            if(cDirInode != NullReference){
+                return cDirInode;
+            }
+            throw SOException(ENOTDIR, __FUNCTION__);
+        }
+        throw SOException(EACCES, __FUNCTION__);
+
     }
 };
 
