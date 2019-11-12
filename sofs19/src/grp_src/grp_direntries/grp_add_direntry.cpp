c37b51ad30f225c89f0f067f251cf1321998f4de
Tomás Freitas <tomasfreitas@ua.pt>
2019-11-12 17:23:45 +0000

    added add_direntry

--- sofs19/src/grp_src/grp_direntries/grp_add_direntry.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_add_direntry.cpp
@@ -15,8 +15,58 @@ namespace sofs19
     {
         soProbe(202, "%s(%d, %s, %u)\n", __FUNCTION__, pih, name, cin);
 
+        SOInode* pi = soGetInodePointer(pih);
+
+        int emptySlot = -1;
+        int emptySlotBlockIndex = -1;
+        SODirEntry emptySlotBlock[DPB];
+
+
+        SODirEntry d[DPB];
+        uint32_t i = 0;
+        for (; i < (pi->size / BlockSize); i++ ) {
+            soReadFileBlock(pih, i, d);
+            uint32_t j = 0;
+            for (; j < DPB; j++) {
+                if (emptySlot < 0 && d[j].name[0] == '\0') {
+                    soReadFileBlock(pih, i, emptySlotBlock);
+                    emptySlotBlockIndex = i;
+                    emptySlot = j;
+                }
+
+                if (!strcmp(d[j].name, name)) {
+                    throw SOException(EEXIST, __FUNCTION__);
+                }
+            }
+        }
+            if (emptySlot >= 0) {
+
+                memcpy(emptySlotBlock[emptySlot].name, name, SOFS19_MAX_NAME+1);
+                memcpy(&emptySlotBlock[emptySlot].in, &cin, sizeof(uint32_t));
+                soWriteFileBlock(pih, emptySlotBlockIndex, emptySlotBlock);
+            }
+            else {
+
+                if (soGetFileBlock(pih, i) == NullReference){
+                    soAllocFileBlock(pih, i);
+                }
+
+                SODirEntry dir[DPB];
+                memset(dir,0,BlockSize);
+                for(uint32_t i = 0; i < DPB; i++){
+                    dir[i].in = NullReference;
+                }
+                memcpy(dir[0].name, name, SOFS19_MAX_NAME+1);
+                dir[0].in = cin;
+                pi->size += BlockSize;
+
+                soWriteFileBlock(pih, i, dir);
+            }
+
+
+
         /* change the following line by your code */
-        binAddDirEntry(pih, name, cin);
+        //binAddDirEntry(pih, name, cin);
     }
 };
 
