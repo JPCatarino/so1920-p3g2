6dfdb7fec4e287720efe569d284654bc495699bb
OscarPimentelOP <oscarpimentel@ua.pt>
2019-11-16 20:21:04 +0000

    added check_dir_empty not tested yet

--- sofs19/src/grp_src/grp_direntries/grp_check_dir_empty.cpp
+++ sofs19/src/grp_src/grp_direntries/grp_check_dir_empty.cpp
@@ -14,9 +14,23 @@ namespace sofs19
     bool grpCheckDirEmpty(int ih)
     {
         soProbe(205, "%s(%d)\n", __FUNCTION__, ih);
+        SOInode* in =  soGetInodePointer(ih);
+        SODirEntry direntries[DPB];
 
+        for(uint32_t i = 0; i<in->size; i++){
+            sofs19::soReadFileBlock(ih, i, direntries);
+            for(uint32_t j = 0; j < (sizeof direntries/sizeof direntries[0]); j++){
+                if((strcmp(direntries[j].name, ".") !=0 || strcmp(direntries[j].name, "..") != 0) && strcmp(direntries[j].name, "\0") !=0){
+                    return false;
+                }
+
+            }
+        }
+        return true;
         /* change the following line by your code */
-        return binCheckDirEmpty(ih);
+        //return binCheckDirEmpty(ih);
+
+
     }
 };
 
