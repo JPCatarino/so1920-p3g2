2dc9055f4fb9727b4e135885f948516f46d66369
OscarPimentelOP <oscarpimentel@ua.pt>
2019-11-17 21:42:36 +0000

    Added Reset Free Data Blocks

--- sofs19/src/grp_src/grp_mksofs/grp_mksofs_RFDB.cpp
+++ sofs19/src/grp_src/grp_mksofs/grp_mksofs_RFDB.cpp
@@ -9,12 +9,27 @@
 
 namespace sofs19
 {
-    void grpResetFreeDataBlocks(uint32_t ntotal, uint32_t itotal, uint32_t nbref)
+     void grpResetFreeDataBlocks(uint32_t ntotal, uint32_t itotal, uint32_t nbref)
     {
         soProbe(607, "%s(%u, %u, %u)\n", __FUNCTION__, ntotal, itotal, nbref);
 
         /* change the following line by your code */
-        binResetFreeDataBlocks(ntotal, itotal, nbref);
+        //binResetFreeDataBlocks(ntotal, itotal, nbref);
+            uint32_t usedBlocks = (itotal / IPB) + 1;
+            uint32_t totalBlocks = nbref + usedBlocks + 1;
+
+            char reset[BlockSize];   
+
+            for(uint32_t i = 0; i< BlockSize; i++){
+                reset[i] = 0;  //coloca a 0 
+            }
+
+            while( totalBlocks < ntotal){
+                soWriteRawBlock(totalBlocks++, reset);
+            }
+
+
+
     }
 };
 
